0dd96d50e270488cd6f30746d286256f34adcd36
yunchao <tanyunchao@gmail.com>
2023-01-05 20:57:44 +0800

    fixed api call

--- pokemon app/poke_project/settings.py
+++ pokemon app/poke_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'poke_project.urls'
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000',
-     'http://127.0.0.1:3000'
+     'http://127.0.0.1:3000',
 ]
 
 REST_FRAMEWORK = {
